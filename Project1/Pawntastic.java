99b1943a405996608f8b2b73ffab58323cb6cf02
CS-Expertjojo <99567651+CS-Expertjojo@users.noreply.github.com>
2023-09-10 13:07:25 -0400

    ask user prefer

---
+++ Project1/Pawntastic.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Pawntastic {
+    public static void main(String[] args){
+        //gameSize 4*4 5*5 8*8
+        Integer[] gameSize = {1, 2, 3};
+        //chessPiece
+        String[] chessPiece = {"O", "X"};
+
+        //--Ask game size from user
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Pawntasitic");
+        System.out.print("Which size you prefer?\n"
+            + "1. 4*4\n"
+            + "2. 5*5\n"
+            + "3. 8*8\n");
+        //todo: check the input from user
+        Integer realSize = Integer.parseInt(sc.nextLine());
+        System.out.println(realSize);
+
+        //--Ask which method from user
+        System.out.println("Which method you prefer?\n"
+            + "1. MINIMAX\n"
+            + "2. H-MINIMAX with α/β");
+        //todo: check the input from user
+        Integer realMethod = Integer.parseInt(sc.nextLine());
+        System.out.println(realMethod);
+
+
+
+
+
+
+
+
+    }
+}
