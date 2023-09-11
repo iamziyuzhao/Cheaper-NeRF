00accf0bd2935f26e27fd6bba6125fc1ab1222d6
CS-Expertjojo <99567651+CS-Expertjojo@users.noreply.github.com>
2023-09-11 12:43:08 -0400

    initial the pawn

--- Project1/InitGame.java
+++ Project1/InitGame.java
@@ -7,9 +7,8 @@ public class InitGame {
     public InitGame(int length){
         this.length = length;
         this.cordnt = new char[length][length];
+        initPawn();
     }
-
-
 
      public void gameBoard(){
          System.out.print("\n");
@@ -42,5 +41,18 @@ public class InitGame {
         }
 
 
+
+        public void initPawn(){
+            if (length == 4){
+                cordnt[1][0]=cordnt[1][1]=cordnt[1][2]=cordnt[1][3]='X';
+                cordnt[2][0]=cordnt[2][1]=cordnt[2][2]=cordnt[2][3]='O';
+            }else if(length == 5){
+                cordnt[1][0]=cordnt[1][1]=cordnt[1][2]=cordnt[1][3]=cordnt[1][4]='X';
+                cordnt[3][0]=cordnt[3][1]=cordnt[3][2]=cordnt[3][3]=cordnt[3][4]='O';
+            }else{
+                cordnt[1][0]=cordnt[1][1]=cordnt[1][2]=cordnt[1][3]=cordnt[1][4]=cordnt[1][5]=cordnt[1][6]=cordnt[1][7]='X';
+                cordnt[6][0]=cordnt[6][1]=cordnt[6][2]=cordnt[6][3]=cordnt[6][4]=cordnt[6][5]=cordnt[6][6]=cordnt[6][7]='O';
+            }
+        }
 
 }
