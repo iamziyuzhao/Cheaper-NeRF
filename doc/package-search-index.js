0906c92c4ea219c783731ffdfcbc70359c4bcdca
自在意 <144565088+iamziyuzhao@users.noreply.github.com>
2025-09-21 14:23:11 -0400

    Cheaper initial add

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"bn.base"},{"l":"bn.core"},{"l":"bn.examples"},{"l":"bn.inference"},{"l":"bn.parser"}];updateSearchResults();
